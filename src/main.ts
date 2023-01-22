70e43e1e9cddc721463a56e5ab8a5cd49878e9a8
Vladyslav Herasymchuk <slaughternal@gmail.com>
2023-01-22 14:05:43 +0200

    15. Inited Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Messages API')
+    .setDescription('The Nest messages API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
